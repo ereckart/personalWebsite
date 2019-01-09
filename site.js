7b994a8d79d3165ecdcfb1f16e262f6e3abc4b98
ereckart <ereckart@seas.upenn.edu>
2019-01-08 23:32:41 -0500

    backend

--- site.js
+++ site.js
@@ -4,5 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Loading.');
 });
+app.get('/projects', (req, res) => {
+    res.sendFile('front_end/projects.html');
+});
 
 app.listen(8001, () => console.log('Personal website listening on port 8001.'));
