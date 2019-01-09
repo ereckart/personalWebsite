986210ad167ec2999851bb3a330a2f6bf531ad1b
Kevin Thomas <thomaskevinlee@gmail.com>
2019-01-08 23:36:04 -0500

    fixed file path for express rendering

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send('Loading.');
 });
 app.get('/projects', (req, res) => {
-    res.sendFile('front_end/projects.html');
+    res.sendFile(__dirname + '/front_end/projects.html');
 });
 
 app.listen(8001, () => console.log('Personal website listening on port 8001.'));
