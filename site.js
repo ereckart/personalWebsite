efc1658c68af960253ea810c591f615eeb5a1cf6
Kevin Thomas <thomaskevinlee@gmail.com>
2019-01-08 23:43:29 -0500

    fixed home url for real

--- site.js
+++ site.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/front_end/'home.html');
+    res.sendFile(__dirname + '/front_end/home.html');
 });
 app.get('/projects', (req, res) => {
     res.sendFile(__dirname + '/front_end/projects.html');
